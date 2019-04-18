32a985030da283bba3a887e04223410cbfcb6faf
Joey Ashcroft <ashcroft.joey@gmail.com>
2019-04-17 19:23:48 -0700

    final changes

--- data_sets_combine.sql
+++ data_sets_combine.sql
@@ -6,9 +6,30 @@ USE world_development_db;
 SELECT count(*) FROM olympic_data;
 SELECT * FROM olympic_data;
 SELECT * FROM world_dev_data;
+SELECT * FROM health_data;
 
 -- Joins the tables
-SELECT count(*)
+
+#SELECT wd.country_name, od.country_name, wd.`year`, od.`year`
+SELECT *
 FROM olympic_data od
-	LEFT JOIN world_dev_data wd ON od.country_name = wd.country_name 
-    AND od.`year` = wd.`year`;
+	LEFT JOIN world_dev_data wd ON wd.country_name = od.country_name 
+    AND wd.`year` = od.`year`
+    LEFT JOIN health_data hd ON hd.country_name = od.country_name
+    AND hd.`year`= od.`year`
+;
+    
+    
+    
+#organize data to prepare for join
+CREATE INDEX idx_country_year on olympic_data(country_name(10), `year`);  
+CREATE INDEX idx_country_year__ on world_dev_data(country_name(10), `year`(4));  
+    
+        
+    
+    
+    
+    
+    
+    
+    
